d96e8bf76c7ea4b073cde4efd214a666cc3d0b4b
Chris Jenkins <chris@redhat.com>
2019-10-01 11:19:37 +0100

    Added column and images to myrti to indicate test/demo assessments

--- myrti.php
+++ myrti.php
@@ -21,6 +21,8 @@ header("Location: login.php");
 <script src="js/jquery-1.12.4.js"></script>
 <script src="js/jquery-ui.js"></script>
 <script src="js/jquery.dataTables.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>  
+
 </head>
 <body>
 
@@ -39,15 +41,13 @@ header("Location: login.php");
 		<div class="collapse navbar-collapse" id="navbar1">
 			<ul class="nav navbar-nav navbar-right">
 				<?php if (isset($_SESSION['usr_id'])) { ?>
+				<li><a href="myrti.php">My RTI</a></li>
 				<li><a href="assess.php">Run Assessment</a></li>
-				<li><p class="navbar-text">Signed in as <?php echo $_SESSION['usr_name']; ?></p></li>
+				<li><a href="#">Signed in as <?php echo $_SESSION['usr_name']; ?></a></li>
 				<li><a href="logout.php">Log Out</a></li>
-				<li><a href="blog">Blog</a></li>
 				<?php } else { ?>
 				<li><a href="register.php">Register</a></li>
 				<li><a href="login.php">Login</a></li>
-				<li><a href="blog">Blog</a></li>
-
 				<?php } ?>
 
 			</ul>
@@ -72,6 +72,7 @@ $userName = $_SESSION['usr_name'];
         <th>Country</th>
         <th>Line of Business</th>
         <th>Timestamp</th>
+        <th>Customer Data</th>
         <th>Link to Output</th>
     </tr>
     </thead>
@@ -81,7 +82,13 @@ connectDB();
 $qq = "SELECT * FROM data WHERE user='".$userName."'";
 $res = mysqli_query($db, $qq);
 while ($row = $res->fetch_assoc()) {
-print "<tr><td>" . $row['client'] . "</td><td>" . $row['country'] . "</td><td>" . $row['lob'] . "</td><td>" . $row['date'] . "</td><td><a target=_ href=results.php?hash=" . $row['hash'] . ">Link</a></td></tr>";
+	if ($row['demo'] == "on") {
+	$demoData = "<img src=images/cross.png>";
+	} else {
+	$demoData = "<img src=images/tick.png>";
+	}
+	
+print "<tr><td>" . $row['client'] . "</td><td>" . $row['country'] . "</td><td>" . $row['lob'] . "</td><td>" . $row['date'] . "</td><td>" . $demoData . "</td><td><a target=_ href=results.php?hash=" . $row['hash'] . ">Link</a></td></tr>";
 }
 
 ?>
