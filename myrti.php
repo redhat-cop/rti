bff239ebe4b2ef42608f887b1155c97bbd22c34b
Chris Jenkins <chris@redhat.com>
2019-10-02 11:46:09 +0100

    Graph added to MyRTI

--- myrti.php
+++ myrti.php
@@ -3,6 +3,7 @@ session_start();
 if(!isset($_SESSION['usr_name'])) {
 header("Location: login.php");
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -22,11 +23,14 @@ header("Location: login.php");
 <script src="js/jquery-ui.js"></script>
 <script src="js/jquery.dataTables.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>  
-
+ <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 </head>
+
+
 <body>
 
-<nav class="navbar navbar-default" role="navigation">
+<nav id="top" class="navbar navbar-default" role="navigation">
 	<div class="container-fluid">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar1">
@@ -54,6 +58,7 @@ header("Location: login.php");
 		</div>
 	</div>
 </nav>
+
     <div class="container">
  
 <?php
@@ -68,7 +73,8 @@ $userName = $_SESSION['usr_name'];
 <table  class="bordered" id="assessmentTable">
     <thead>
     <tr>
-        <th>Client Name</th>        
+        <th>Client Name</th>  
+        <th>Email Address</th>      
         <th>Country</th>
         <th>Line of Business</th>
         <th>Timestamp</th>
@@ -88,16 +94,94 @@ while ($row = $res->fetch_assoc()) {
 	$demoData = "<img src=images/tick.png>";
 	}
 	
-print "<tr><td>" . $row['client'] . "</td><td>" . $row['country'] . "</td><td>" . $row['lob'] . "</td><td>" . $row['date'] . "</td><td>" . $demoData . "</td><td><a target=_ href=results.php?hash=" . $row['hash'] . ">Link</a></td></tr>";
+print "<tr><td>" . $row['client'] . "</td><td>" . $row['rhEmail'] . "</td><td>" . $row['country'] . "</td><td>" . $row['lob'] . "</td><td>" . $row['date'] . "</td><td>" . $demoData . "</td><td><a target=_ href=results.php?hash=" . $row['hash'] . ">Link</a></td></tr>";
 }
+
+#$q1 = "select lob, count(*) as total from data where demo <> 'on' group by lob order by total desc ;";
+#$sth = mysqli_query($db, $q1);
 
 ?>
 <tbody>
 </table>
+<a href="#columnchart12">Scroll to Graphs</a>
 
+
+<script type="text/javascript" >
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+</script>
+
+ <script type="text/javascript">
+ google.load("visualization", "1", {packages:["corechart"]});
+ google.setOnLoadCallback(drawChart);
+ function drawChart() {
+ var data = google.visualization.arrayToDataTable([
+ 
+ ['class Name','Students'],
+ <?php 
+			#$query = "SELECT * from class";
+ 			$query = "select lob, count(*) as total from data where user=\"$userName\" and demo <> 'on' group by lob order by total desc ;";
+			 $exec = mysqli_query($db,$query);
+			 while($row = mysqli_fetch_array($exec)){
+ 
+			 echo "['".$row['lob']."',".$row['total']."],";
+			 }
+			 ?> 
+ 
+ ]);
+ 
+ var options = {
+ title: 'Breakdown by Line of Business',
+  pieHole: 0.5,
+          pieSliceTextStyle: {
+            color: 'black',
+          },
+          legend: 'XXXX'
+ };
+ var chart = new google.visualization.PieChart(document.getElementById("columnchart12"));
+ chart.draw(data,options);
+ }
+	
+    </script>
+    
+<!--     <h2 id="graphs" style="padding-top: 500px;">Graphs</h2> -->
+ <div id="columnchart12" style="width: 100%; height: 500px;"></div>
+<a href="#top">Scroll to Top</a>    
       </div>
-
-
     </div> <!-- /container -->
 <?php    }
 ####  End of Logged on bit ######
@@ -108,5 +192,6 @@ $(document).ready( function () {
     $('#assessmentTable').DataTable();
 } );
 </script> 
+
 </body>
 </html>
